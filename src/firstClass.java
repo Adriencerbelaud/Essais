65082b4aa3f1014054137727b6df36a655f56622
AdrienCerbelaud <adriencerbelaud@hotmail.com>
2017-10-17 16:24:02 +0200

    concatene str int

--- src/firstClass.java
+++ src/firstClass.java
@@ -2,17 +2,9 @@
 public class firstClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String salut = "Bonjour";
-		String qui = " à tous";
-		System.out.println(salut + qui);
-		
-		String tab[] = {"Salut", " à tous"};
-		System.out.println(tab[0] + tab[1]);
-		
-		for (String each : tab){
-			System.out.print(each);
-		}
+		String tab[] = {"Tu as ", " ans"};
+		int age = 18;
+		System.out.print(tab[0] + age + tab[1]);
 	}
 
 }
